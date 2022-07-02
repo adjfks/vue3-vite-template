1fe9f44d0911ce98e3300ea439f995985b72d7c6
corner <sptdfdzl@163.com>
2022-07-02 13:03:34 +0800

    chore: add pinia

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-
+import { useIndex } from '@/stores/index'
+const index = useIndex()
 </script>
 
 <template>
   <div class="hello-container">
     <h1>hello word</h1>
+    <p>{{ index.id }}</p>
   </div>
 </template>
 
---
+++ src/stores/index.ts
@@ -0,0 +1,15 @@
+import { defineStore } from 'pinia'
+
+export const useIndex = defineStore('index', {
+  state: () => {
+    return {
+      id: 123
+    }
+  },
+  getters: {
+
+  },
+  actions: {
+
+  }
+})
