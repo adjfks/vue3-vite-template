fc7efe0dbd18cba53f5f24da8cf38b4a4a784872
corner <sptdfdzl@163.com>
2022-07-02 16:21:33 +0800

    chore: add stylelint

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,11 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  margin-top: 60px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
